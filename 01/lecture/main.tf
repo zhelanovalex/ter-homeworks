f5fb9ac9bec4bb5878fabcea1f556ff2f8b539f0
Zhelanovalex <zhelanov@gmail.com>
2026-01-16 16:22:02 +0300

    qx

--- 01/lecture/main.tf
+++ 01/lecture/main.tf
@@ -1,15 +1,15 @@
 terraform {
   required_providers {}
-  required_version = "~>1.12.0"
+  required_version = ">1.14.0"
 }
 
-resource "random_password" "any_uniq_name" {
+resource "random_password" "neto_test_name" {
   length = 16
 }
 
 resource "local_file" "from_resourse" {
-  content  = random_password.any_uniq_name.result
-  filename = "/tmp/from_resource.txt"
+  content  = random_password.neto_test_name.result
+  filename = "/tmp/from_resource_neto.txt"
 }
 
 
@@ -19,7 +19,17 @@ data "local_file" "version" {
   filename = "/proc/version"
 }
 
-resource "local_file" "from_dataresourse" {
+resource "local_file" "from_dataversion" {
   content  = data.local_file.version.content
-  filename = "/tmp/from_data_source.txt"
+  filename = "/tmp/from_data_source_version_neto.txt"
 }
+
+data "local_file" "list_hosts" {
+  filename = "/etc/hosts"
+}
+
+resource "local_file" "from_datahosts" {
+  content  = data.local_file.list_hosts.content
+  filename = "/tmp/from_data_source_hosts_neto.txt"
+}
+
